fbbf0834ff2435c5e0745d52e8ecd1acdbc25a9c
mockysunshinework <mockysunshinework@gmail.com>
2022-12-07 19:27:51 +0900

    日本語化実装。レイアウトとdash_boardsのURLを調査中

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :users do
+    resource :dash_boards, only: :show, as: :root
+  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: {
     registrations: 'users/registrations',
