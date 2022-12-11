4e2bf66cccf0db362bdb287e39bbdec50ff002fb
mockysunshinework <mockysunshinework@gmail.com>
2022-12-11 19:07:47 +0900

    routes.rbにresources :articles を記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
+  root 'home#top'
   namespace :users do
     resource :dash_boards, only: :show, as: :root
+    resources :articles
+
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
   }
-  root 'home#top'
+  
+  
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
