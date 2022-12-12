d75640953b02b878a18677ccb69cda4ebc87d19f
mockysunshinework <mockysunshinework@gmail.com>
2022-12-12 17:28:05 +0900

    devise.views.ja.ymlを編集。

--- app/controllers/users/articles_controller.rb
+++ app/controllers/users/articles_controller.rb
@@ -1,7 +1,28 @@
 class Users::ArticlesController < ApplicationController
   def new
+    @article = current_user.articles.new
   end
+
+  def create
+    @article = current_user.articles.new(article_params)
+    
+    if @article.save
+      flash[:success] = "新規作成に成功しました"
+      redirect_to users_articles_path
+    else
+      render :new
+    end
+  end
+
   def index
-    @articles = Article.all
+    @articles = current_user.articles.all
   end
+
+
+private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
